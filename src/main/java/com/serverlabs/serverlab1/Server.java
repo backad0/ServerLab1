46d76b0faca2efcd3551f2383e6cfd55d1346782
backad0 <aleshkinalesh1@gmail.com>
2023-12-06 00:04:53 +0600

    delete comment

--- src/main/java/com/serverlabs/serverlab1/Server.java
+++ src/main/java/com/serverlabs/serverlab1/Server.java
@@ -32,7 +32,7 @@ public class Server implements IServer {
     private static GroupController groupController;
 
     static {
-        Database database = new Database(new HashMap<Long, StudentDB>(), new HashMap<Long, Group>());
+        Database database = new Database(new HashMap<>(), new HashMap<>());
         StudentService studentService = new StudentService(new StudentRepo(database));
         GroupService groupService = new GroupService(new GroupRepo(database));
         studentController = new StudentController(studentService, new AddStudentValidator(), new DeleteStudentValidator(),
@@ -65,80 +65,3 @@ public class Server implements IServer {
         }
     }
 }
-/*ObjectMapper mapper = new ObjectMapper();
-        switch (endPoint) {
-            case ("add student"):
-                try {
-                    ResponseEntity<CommonResponse<AddStudentResponse>> res = studentController.
-                            addStudent(mapper.readValue(json, AddStudentRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("delete student"):
-                try {
-                    ResponseEntity<CommonResponse<Void>> res = studentController.
-                            deleteStudent(mapper.readValue(json, DeleteStudentRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("edit student"):
-                try {
-                    ResponseEntity<CommonResponse<Void>> res = studentController.
-                            editStudent(mapper.readValue(json, EditStudentRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("get student by id"):
-                try {
-                    ResponseEntity<CommonResponse<GetStudentByIdResponse>> res = studentController.
-                            getStudentById(mapper.readValue(json, GetStudentByIdRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("get students by group"):
-                try {
-                    ResponseEntity<CommonResponse<List<Student>>> res = studentController.
-                            getStudentsByGroup(mapper.readValue(json, GetStudentsByGroupRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("add group"):
-                try {
-                    ResponseEntity<CommonResponse<AddGroupResponse>> res = groupController.
-                            addGroup(mapper.readValue(json, AddGroupRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("delete group"):
-                try {
-                    ResponseEntity<CommonResponse<Void>> res = groupController.
-                            deleteGroup(mapper.readValue(json, DeleteGroupRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("edit group"):
-                try {
-                    ResponseEntity<CommonResponse<Void>> res = groupController.
-                            editGroup(mapper.readValue(json, EditGroupRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            case ("get group by id"):
-                try {
-                    ResponseEntity<CommonResponse<GetGroupByIdResponse>> res = groupController.
-                            getGroupById(mapper.readValue(json, GetGroupByIdRequest.class));
-                    return mapper.writeValueAsString(res);
-                } catch (Exception e) {
-                    throw new ServerException(e.getMessage());
-                }
-            default:
-                throw new ServerException("No such endpoint");
-        }*/
